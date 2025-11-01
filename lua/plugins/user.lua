c876766cf316f5f0bffbdce9396bf6690972232d
jeanmarcboite <jeanmarc.boite@gmail.com>
2025-11-01 17:40:06 +0100

    updates

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- PLEASE REMOVE THE EXAMPLES YOU HAVE NO INTEREST IN BEFORE ENABLING THIS FILE
 -- Here are some examples:
@@ -84,5 +82,13 @@ return {
         Rule("a", "a", "-vim")
       )
     end,
+  },
+
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function() vim.g.mkdp_filetypes = { "markdown" } end,
+    ft = { "markdown" },
   },
 }
